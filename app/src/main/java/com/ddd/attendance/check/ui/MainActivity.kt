5e1f72be80e768b9b1f15a78a254f071c59e0c17
mkw8263 <ruddns8213@naver.com>
2019-01-13 20:26:02 +0900

    개발환경 세팅

---
+++ app/src/main/java/com/ddd/attendance/check/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ddd.attendance.check.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ddd.attendance.check.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
