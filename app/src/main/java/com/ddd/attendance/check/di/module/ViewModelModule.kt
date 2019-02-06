4c411d9a73051519af05e2bbb9f9f7f9b40f65cb
mkw8263 <ruddns8213@naver.com>
2019-02-06 22:22:59 +0900

    Dagger, ViewModel Module 추가

--- app/src/main/java/com/ddd/attendance/check/di/module/ViewModelModule.kt
+++ app/src/main/java/com/ddd/attendance/check/di/module/ViewModelModule.kt
@@ -1,11 +1,19 @@
 package com.ddd.attendance.check.di.module
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.ddd.attendance.check.viewModel.MainViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
     @Binds
-    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindMainViewModel(viewModel: MainViewModel): ViewModel
 }
--- app/src/main/java/com/ddd/attendance/check/ui/MainActivity.kt
+++ app/src/main/java/com/ddd/attendance/check/ui/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ddd.attendance.check.ui
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.ddd.attendance.check.R
 import com.ddd.attendance.check.base.DDDActivity
 import com.ddd.attendance.check.databinding.ActivityMainBinding
+import com.ddd.attendance.check.utill.lazyThreadSafetyNone
+import com.ddd.attendance.check.viewModel.MainViewModel
+import javax.inject.Inject
 
 class MainActivity : DDDActivity<ActivityMainBinding>() {
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+    val viewModel by lazyThreadSafetyNone { ViewModelProviders.of(this, factory)[MainViewModel::class.java] }
+
     override val layoutResource: Int
         get() = R.layout.activity_main
 
@@ -16,7 +25,8 @@ class MainActivity : DDDActivity<ActivityMainBinding>() {
 
     private fun setDataBinding() {
         viewDataBinding.apply {
-
+            viewModel = this.viewModel
+            setLifecycleOwner(this@MainActivity)
         }
     }
 }
