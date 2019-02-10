3b6fba3ba6d3e9dd2bc3b77c19fec4b0fe2da7b8
mkw8263 <ruddns8213@naver.com>
2019-02-10 21:31:33 +0900

    출석 확인 dialog custom 완료

--- app/src/main/java/com/ddd/attendance/check/ui/MainActivity.kt
+++ app/src/main/java/com/ddd/attendance/check/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.ddd.attendance.check.ui
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.ddd.attendance.check.R
 import com.ddd.attendance.check.base.DDDActivity
+import com.ddd.attendance.check.common.DDDDialog
 import com.ddd.attendance.check.databinding.ActivityMainBinding
 import com.ddd.attendance.check.utill.lazyThreadSafetyNone
 import com.ddd.attendance.check.vm.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 
@@ -21,6 +24,18 @@ class MainActivity : DDDActivity<ActivityMainBinding, MainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setDataBinding()
+        setUpDialog()
+    }
+
+    private fun setUpDialog() {
+        btnAttendance.setOnClickListener {
+            DDDDialog(this)
+                .setDialogListener(object : DDDDialog.DDDDialogEventListener {
+                    override fun onClick(dialog: Dialog) {
+                        dialog.dismiss()
+                    }
+                }).show()
+        }
     }
 
     private fun setDataBinding() {
